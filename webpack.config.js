033c41c87d8cc52afb9285e854221171f6f1e9a1
Justin Higgs <justinphiggs@gmail.com>
2021-08-12 19:13:45 -0400

    Added images from course repo. Disabled CopyWebpackPlugin since coming to part of course where using Asset Modules. Cleaned notes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
-const path = require( 'path' ); // 'path' is a Node core module. Have to require/import it but it's always there.
-const CopyPlugin = require( 'copy-webpack-plugin' );
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-let mode = 'development';
-let target = 'web'; // Bug with either webpack-dev-server or Webpack 5 itself where having a .browserslistrc will fuck up live reloading (of at least [S]CSS). Manually specifying the target prop will fix it until an official fix is patched in.
+// NOTE: 'path' is a Node core module. Have to require/import it but it's always there.
+// NOTE: See note about CopyWebpackPlugin below in plugins object.
+
+const path = require( 'path' ),
+// CopyPlugin = require( 'copy-webpack-plugin' ),
+MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// Bug with either webpack-dev-server or Webpack 5 itself where having a .browserslistrc will fuck up live reloading (of at least [S]CSS). Manually specifying the target prop will fix it until an official fix is patched in.
+let mode = 'development',
+target = 'web';
 
  if ( process.env.NODE_ENV === 'production' ) {
-   mode = 'production';
+   mode   = 'production';
    target = 'browserslist';
  }
 
@@ -18,6 +23,7 @@ module.exports = {
   module: {
     rules: [
       {
+        //* Regex: placing a `?` after a char makes it optional; this ~= `(js|jsx)`.
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
@@ -25,7 +31,9 @@ module.exports = {
         }
       },
       {
-        test: /\.(sc|c)ss$/i, //* Can be written as `(scss|css)`, `(s(a|c)|c)ss`, `s?css`, or `(s[ac]|c)ss`. Regex is fun!
+        //* Can be written as `(scss|css)`, `(s(a|c)|c)ss`, `s?css`, or `(s[ac]|c)ss`. Regex is fun!
+        test: /\.(sc|c)ss$/i,
+        // NOTE: Even though these are listed LTR, the shit is processed through these RTL, so sass then postcss, then css, then MiniCssExtractPlugin.
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
@@ -35,31 +43,42 @@ module.exports = {
       },
     ],
   },
+
   resolve: {
     extensions: ['.js', '.jsx']
   },
+
+  devtool: 'source-map', // Annoying that this is all lowercase and following is camel.
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' },
+    //* Hot Module Replacement (HMR); here, specifically for CSS live-reload.
     hot: true,
-    // contentBase: "./dist", //* Works fine. If you needed absolute instead of relative, use below instead:
+    //* Works fine. If you needed absolute instead of relative, use below instead:
+    // contentBase: "./dist",
     contentBase: path.join( __dirname, 'dist' ),
-    // public: "localhost:8080" // ! This is the default setup (technically, host: "localhost" is the issue I think)
-    // public: "127.0.0.1:8080", // * This works. So something about using 'localhost' instead of the localhost IP makes WDS confused. Manually setting host and port below for clarity instead.
+    // ! This is the default setup, not necess. the properties its set on.
+    // public: "localhost:8080",
+    //* This works. So something about using 'localhost' instead of the localhost IP
+    //* makes WDS confused. Manually setting host and port below for clarity instead.
+    // public: "127.0.0.1:8080",
     // host: "127.0.0.1", //! Doesn't work.
     // host: "localhost", //! Doesn't work.
     port: 8080,
+    //* I think this was to resolve the `[WDS] Disconnected!` error. Helped some but not 100%.
     disableHostCheck: true
   },
-  devtool: 'source-map',
+
   plugins: [
-    new CopyPlugin( {
-      patterns: [
-        {
-          from: 'src/images',
-          to: 'img',
-        }
-      ],
-    } ),
+    // NOTE: I added this early on so that I could have plain old passthrough copying of static images to dist/ without importing through JS. Disabling since I've gotten to part in course where he's doing the standard, imported-by-JS setup with Webpack 5's Asset Modules, which I tried at first. My original images are in oldimages folder.
+    // NOTE: If you want to reinstall, the package is `"copy-webpack-plugin": "^9.0.1"`.
+    // new CopyPlugin( {
+    //   patterns: [
+    //     {
+    //       from: 'src/images',
+    //       to: 'img',
+    //     }
+    //   ],
+    // } ),
     new MiniCssExtractPlugin(),
   ],
 };
