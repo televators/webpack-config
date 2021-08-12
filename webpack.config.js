3ba68c0f50975254ee563509e4cb9b0aa958410e
Justin Higgs <justinphiggs@gmail.com>
2021-08-12 16:53:38 -0400

    Added browserslistrc for the PostCss Autoprefixer, more webpack options, and a fix for live reloading because of the browserslistrc.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require( 'path' ); // 'path' is a Node core module. Have to require/import it but it's always there.
 const CopyPlugin = require( 'copy-webpack-plugin' );
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 let mode = 'development';
+let target = 'web'; // Bug with either webpack-dev-server or Webpack 5 itself where having a .browserslistrc will fuck up live reloading (of at least [S]CSS). Manually specifying the target prop will fix it until an official fix is patched in.
 
  if ( process.env.NODE_ENV === 'production' ) {
    mode = 'production';
+   target = 'browserslist';
  }
 
 module.exports = {
   mode: mode,
-  stats: 'errors-warnings',
+  target: target,
+  // stats: 'errors-warnings', //* Use this to squash most of the output in CLI.
+  stats: 'normal',
 
   module: {
     rules: [
@@ -19,10 +24,20 @@ module.exports = {
           loader: 'babel-loader',
         }
       },
+      {
+        test: /\.(sc|c)ss$/i, //* Can be written as `(scss|css)`, `(s(a|c)|c)ss`, `s?css`, or `(s[ac]|c)ss`. Regex is fun!
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]
+      },
     ],
   },
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' },
+    hot: true,
     // contentBase: "./dist", //* Works fine. If you needed absolute instead of relative, use below instead:
     contentBase: path.join( __dirname, 'dist' ),
     // public: "localhost:8080" // ! This is the default setup (technically, host: "localhost" is the issue I think)
@@ -42,5 +57,6 @@ module.exports = {
         }
       ],
     } ),
+    new MiniCssExtractPlugin(),
   ],
 };
